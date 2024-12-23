d88dfb86960bff6350110904eaec3cb2ae327bb4
bishnuf66 <bkbishnu2000@gmail.com>
2024-12-23 13:32:52 +0545

    updated seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,17 @@ import Membership from "@/components/Membership";
 import Program from "@/components/Program";
 import Testimonials from "@/components/Testimonials";
 import WhyUs from "@/components/WhyUs";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="  font-[family-name:var(--font-geist-sans)]">
-      <link
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-        rel="stylesheet"
-      />
+      <Head>
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+          rel="stylesheet"
+        />
+      </Head>
       <HeroSection />
       <AboutUs />
       <Program />
